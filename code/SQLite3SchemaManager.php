12ac98ec1a7db26d82f82e39db5235c5d441f9a8
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2017-10-10 13:27:20 +0200

    fix undefined SapphireTest::using_temp_db()
    see #33

--- code/SQLite3SchemaManager.php
+++ code/SQLite3SchemaManager.php
@@ -210,7 +210,7 @@ class SQLite3SchemaManager extends DBSchemaManager
     {
         $ok = true;
 
-        if (!SapphireTest::using_temp_db() && !self::$checked_and_repaired) {
+        if (SapphireTest::getTempDB()->isUsed() && !self::$checked_and_repaired) {
             $this->alterationMessage("Checking database integrity", "repaired");
 
             // Check for any tables with failed integrity
